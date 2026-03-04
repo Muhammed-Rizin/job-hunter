be2bd0cd73ef68b3e99abf584d502d1542b00e0f
muhammed-rizin <rizin7427@gmail.com>
2026-03-05 05:21:34 +0530

    Status

--- controller/stats.controller.js
+++ controller/stats.controller.js
@@ -1,31 +1,27 @@
 import models from "../model/index.js";
-import asyncErrorHandler from "../middleware/asyncErrorHandler.js";
-import Response from "../utils/responseHandler.js";
 
 export const getCounts = asyncErrorHandler(async (req, res) => {
   const userId = req.user._id;
-  const todayStr = new Date().toLocaleDateString('en-CA', { timeZone: 'Asia/Kolkata' });
+  const todayStr = new Date().toISOString().split("T")[0];
 
   const [allApps, plans] = await Promise.all([
     models.Application.find({ user: userId, statusFlag: 0 }),
     models.Plan.find({ user: userId, statusFlag: 0 }),
   ]);
 
-  const pendingLeads = plans.filter(p => p.status === "pending").length;
-  const appliedLeads = plans.filter(p => p.status === "applied").length;
-  
-  const bouncedApps = allApps.filter(a => a.status === "bounced").length;
-  const totalApplied = allApps.length - bouncedApps;
-  const offerApps = allApps.filter(a => a.status === "offer").length;
-  const appsToday = allApps.filter(a => a.appliedDate === todayStr).length;
+  const pendingApps = plans.filter((p) => p.status === "pending").length;
+  const bouncedApps = allApps.filter((a) => a.status === "bounced").length;
+  const appliedApps = allApps.length - bouncedApps;
+  const offerApps = allApps.filter((a) => a.status === "offer").length;
+  const appsToday = allApps.filter((a) => a.appliedDate === todayStr).length;
 
   return new Response(
     "Counts fetched",
     {
-      totalApplied,
-      pendingLeads,
-      appliedLeads,
+      totalApps: appliedApps,
+      appliedApps,
       bouncedApps,
+      pendingApps,
       offerApps,
       appsToday,
     },
